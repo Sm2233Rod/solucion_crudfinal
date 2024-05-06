535aca68e17219d6f179b5acd65a691196c133c3
Sm2233Rod <168204139+Sm2233Rod@users.noreply.github.com>
2024-05-06 07:46:25 -0600

    Add files via upload

---
+++ FormClientes.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace solucion_crud
+{
+    partial class FormClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Buscar = new System.Windows.Forms.Button();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.apellido = new System.Windows.Forms.Label();
+            this.telefono = new System.Windows.Forms.Label();
+            this.ciud = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.cancel = new System.Windows.Forms.Button();
+            this.saved = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(527, 256);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Buscar";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(466, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(12, 311);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 3;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(93, 311);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 4;
+            this.Delete.Text = "Delete";
+            this.Delete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(174, 311);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(75, 23);
+            this.Update.TabIndex = 5;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(255, 311);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 6;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            // 
+            // Buscar
+            // 
+            this.Buscar.Location = new System.Drawing.Point(545, 5);
+            this.Buscar.Name = "Buscar";
+            this.Buscar.Size = new System.Drawing.Size(75, 20);
+            this.Buscar.TabIndex = 7;
+            this.Buscar.Text = "Buscar";
+            this.Buscar.UseVisualStyleBackColor = true;
+            this.Buscar.Click += new System.EventHandler(this.Buscar_Click);
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(578, 78);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(44, 13);
+            this.Nombre.TabIndex = 8;
+            this.Nombre.Text = "Nombre";
+            // 
+            // apellido
+            // 
+            this.apellido.AutoSize = true;
+            this.apellido.Location = new System.Drawing.Point(585, 137);
+            this.apellido.Name = "apellido";
+            this.apellido.Size = new System.Drawing.Size(44, 13);
+            this.apellido.TabIndex = 9;
+            this.apellido.Text = "Apellido";
+            this.apellido.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // telefono
+            // 
+            this.telefono.AutoSize = true;
+            this.telefono.Location = new System.Drawing.Point(585, 194);
+            this.telefono.Name = "telefono";
+            this.telefono.Size = new System.Drawing.Size(45, 13);
+            this.telefono.TabIndex = 10;
+            this.telefono.Text = "telefono";
+            // 
+            // ciud
+            // 
+            this.ciud.AutoSize = true;
+            this.ciud.Location = new System.Drawing.Point(587, 249);
+            this.ciud.Name = "ciud";
+            this.ciud.Size = new System.Drawing.Size(40, 13);
+            this.ciud.TabIndex = 11;
+            this.ciud.Text = "Ciudad";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(545, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ShortcutsEnabled = false;
+            this.textBox2.Size = new System.Drawing.Size(194, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(545, 153);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(194, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(545, 210);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(194, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(545, 265);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(194, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(664, 358);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 16;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Visible = false;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // saved
+            // 
+            this.saved.Location = new System.Drawing.Point(664, 329);
+            this.saved.Name = "saved";
+            this.saved.Size = new System.Drawing.Size(75, 23);
+            this.saved.TabIndex = 17;
+            this.saved.Text = "Saved";
+            this.saved.UseVisualStyleBackColor = true;
+            this.saved.Visible = false;
+            this.saved.Click += new System.EventHandler(this.saved_Click);
+            // 
+            // FormClientes
+            // 
+            this.ClientSize = new System.Drawing.Size(751, 435);
+            this.Controls.Add(this.saved);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.ciud);
+            this.Controls.Add(this.telefono);
+            this.Controls.Add(this.apellido);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.Buscar);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FormClientes";
+            this.Text = "formClientes";
+            this.Load += new System.EventHandler(this.FormClientes_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource clientesBindingSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Buscar;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.Label apellido;
+        private System.Windows.Forms.Label telefono;
+        private System.Windows.Forms.Label ciud;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.Button saved;
+    }
+}
---
+++ FormClientes.cs
@@ -0,0 +1,217 @@
+﻿using solucion_crud.logica;
+using solucion_crud.modeloaddup;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace solucion_crud
+{
+    public partial class FormClientes : Form
+    {
+        public FormClientes()
+        {
+            InitializeComponent();
+        }
+
+        #region "mis variables"
+        int nEstadoguarda = 0;
+        string nom;
+        string ape;
+        int num;
+        string ciuda;
+
+        #endregion
+        
+
+        #region "mis metodos"
+        private void Estado_texto(bool lEstado) 
+        {
+            textBox2.ReadOnly = !lEstado;
+            textBox3.ReadOnly = !lEstado;
+            textBox4.ReadOnly = !lEstado;
+            textBox5.ReadOnly = !lEstado;
+        }
+
+        private void limpia_texto()
+        {
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+
+        private void Estado_botonupdate(bool lEstado)
+        {
+            saved.Visible = lEstado;
+            cancel.Visible = lEstado;
+
+            Buscar.Enabled = !lEstado;
+            textBox1.Enabled = !lEstado;
+        }
+
+        private void Estado_btnprincipales(bool lEstado)
+        {
+            add.Enabled = lEstado;
+            Update.Enabled = lEstado;
+            Delete.Enabled = lEstado;
+            Exit.Enabled = lEstado;
+        }
+
+        private void Selecciona_item_cliente()
+        {
+
+        }
+
+       
+
+        #endregion
+        private void listadoclientes()
+        {
+            L_clientes Datos = new L_clientes();
+            dataGridView1.DataSource = Datos.listado_clientes();
+        }
+ 
+
+        private void FormClientes_Load_1(object sender, EventArgs e)
+        {
+            this.listadoclientes();
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            nEstadoguarda = 1; //nuevo registro
+            this.Estado_texto(true);
+            this.limpia_texto();
+            this.Estado_botonupdate(true);
+            this.Estado_btnprincipales(false);
+            textBox2.Focus();
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.Estado_texto(false);
+            this.limpia_texto();
+            this.Estado_botonupdate(false);
+            this.Estado_btnprincipales(true);
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+
+           
+
+            if (dataGridView1.SelectedRows.Count <= 0)
+            {
+                MessageBox.Show("no se tiene registro para actualizar",
+                    "aviso del sistema",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+
+                // Obtener los datos del cliente seleccionado en el DataGridView
+                string nombrecliente = dataGridView1.SelectedRows[0].Cells["nombrecliente"].Value.ToString();
+                string apellidocliente = dataGridView1.SelectedRows[0].Cells["apellidocliente"].Value.ToString();
+
+                // Obtener el ID del cliente
+                L_clientes datosClientes = new L_clientes();
+                int idcliente = datosClientes.ObtenerIdCliente(nombrecliente, apellidocliente);
+
+                if (idcliente != -1)
+                {
+
+                    Selecciona_item_cliente();
+
+                    // Actualizar al cliente con el ID obtenido
+                    nEstadoguarda = 2; // Indicar actualización de registro
+                    textBox2.Text = nombrecliente;
+                    textBox3.Text = apellidocliente;
+
+                    this.limpia_texto();
+                    this.Estado_botonupdate(true);
+                    this.Estado_btnprincipales(false);
+                    this.Estado_texto(true);
+                    textBox2.Focus();
+
+                }
+                else
+                {
+                    MessageBox.Show("El cliente seleccionado no pudo ser encontrado.",
+                                    "Aviso del sistema",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                }
+                
+            }
+        }
+       
+        
+
+        private void saved_Click(object sender, EventArgs e)
+        {
+            string Rpta = "";
+            R_clientes oCi = new R_clientes();
+
+            oCi.nombrecliente = nom;
+            oCi.nombrecliente = textBox2.Text.Trim();
+            oCi.apellidocliente = ape;
+            oCi.apellidocliente = textBox3.Text.Trim();
+
+            if (int.TryParse(textBox4.Text.Trim(), out int telefono))
+            {
+                oCi.telefono = telefono; // Asigna el valor de teléfono si la conversión es exitosa
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un número de teléfono válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // Sale del método si la conversión falla
+            }
+
+            oCi.ciudad = ciuda;
+            oCi.ciudad = textBox5.Text.Trim();
+
+            L_clientes Datos = new L_clientes();
+            Rpta = Datos.Guardar_clientes(nEstadoguarda,oCi);
+
+            if (Rpta.Equals("ok"))
+            {
+                this.listadoclientes();
+                MessageBox.Show("los datos han sido guardados exitosamente",
+                    "aviso del sistema",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(Rpta);
+            }
+
+            if (nEstadoguarda==2)
+            {
+
+            }
+
+            this.Estado_texto(false);
+            this.limpia_texto();
+            this.Estado_botonupdate(false);
+            this.Estado_btnprincipales(true);
+
+
+
+        }
+
+        private void Buscar_Click(object sender, EventArgs e)
+        {
+            this.listadoclientes(textBox1.Text.Trim());
+        }
+    }
+}
+
+    
