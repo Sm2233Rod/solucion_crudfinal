3b958dcf08d76927319c5f465581c6026cc18efc
Sm2233Rod <168204139+Sm2233Rod@users.noreply.github.com>
2024-05-06 09:20:37 -0600

    Add files via upload

---
+++ R_clientes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solucion_crud.modeloaddup
+{
+    public class R_clientes
+    {
+        public string nombrecliente { get; set; }
+        public string apellidocliente { get; set; }
+        public int telefono { get; set; }
+        public string ciudad { get; set; }
+         public int idcliente { get; set; }
+
+    }
+}
